4717832ded2313cfd635ddf5b1aa1fb1929296ea
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-10-06 16:34:18 +0700

    updated chap02 2.1

--- chap02/js/app.js
+++ chap02/js/app.js
@@ -20,43 +20,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const polylineTable = document.getElementById("polylineTable").getElementsByTagName('tbody')[0];
 
     fetch("data.json")
-        .then(response => response.json())
-        .then(data => {
-            let countPoint = 1;
-            let countPolygon = 1;
-            let countPolyline = 1;
+    .then(response => response.json())
+    .then(data => {
+        const features = geoJSONToFeatures(data);
 
-            data.features.forEach(feature => {
-                const row = document.createElement("tr");
-                const namaCell = document.createElement("td");
-                const kordinattesCell = document.createElement("td");
-                const tipeCell = document.createElement("td");
-
-                namaCell.innerText = feature.properties.nama;
-                kordinattesCell.innerText = JSON.stringify(feature.geometry.coordinates);
-                tipeCell.innerText = feature.geometry.type;
-
-                row.appendChild(document.createElement("th"));
-                row.appendChild(namaCell);
-                row.appendChild(kordinattesCell);
-                row.appendChild(tipeCell);
-
-                if (feature.geometry.type === "Point") {
-                    row.getElementsByTagName("th")[0].innerText = countPoint;
-                    pointTable.appendChild(row);
-                    countPoint++;
-                } else if (feature.geometry.type === "Polygon") {
-                    row.getElementsByTagName("th")[0].innerText = countPolygon;
-                    polygonTable.appendChild(row);
-                    countPolygon++;
-                } else if (feature.geometry.type === "LineString") {
-                    row.getElementsByTagName("th")[0].innerText = countPolyline;
-                    polylineTable.appendChild(row);
-                    countPolyline++;
-                }
-            });
-        })
-        .catch(error => console.error("Err:", error));
+            // Loop through the features and add them to the appropriate vector layer.
+        features.forEach(feature => {
+            if (feature.getGeometry().getType() === 'Point') {
+                // Add the feature to the point layer
+                layerpoint.getSource().addFeature(feature);
+            } else if (feature.getGeometry().getType() === 'Polygon' || feature.getGeometry().getType() === 'LineString') {
+                // Add the feature to the polyline or polygon layer
+                polylayer.getSource().addFeature(feature);
+            }
+        });
+    })
+    .catch(error => console.error("Err:", error));
 });
 
 
